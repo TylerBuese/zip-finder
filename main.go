b1a6ecdd3dab3b7a92cccf118d74efa0754630ef
Tyler Buese <tylerbuese@gmail.com>
2023-08-13 14:41:06 -0500

    added web server and serving all zips

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
 type ZipCodes struct {
 	Zip    string
 	City   string
@@ -12,5 +19,22 @@ type ZipCodes struct {
 func main() {
 	path := "./zip_code_database.csv"
 	zips := ReadFile(path)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		text, marshalErr := json.Marshal(zips)
+		if marshalErr != nil {
+			log.Println(marshalErr)
+		}
+
+		_, err := fmt.Fprintf(w, string(text))
+		log.Println(r.Method)
+
+		if err != nil {
+
+		}
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
 
 }
